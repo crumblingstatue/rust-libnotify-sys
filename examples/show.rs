4442eadafb44e6058987433e2d8695032bfd9e18
Mika Attila <radiantstatue@gmail.com>
2016-02-10 09:33:33 +0100

    Make example more idiomatic

--- examples/show.rs
+++ examples/show.rs
@@ -4,18 +4,20 @@ extern crate glib_2_0_sys as glib;
 fn main() {
     unsafe {
         use libnotify::*;
-        use std::mem::transmute as tm;
+        use std::os::raw::c_char;
+        use std::ptr;
 
-        if notify_init(tm("hello\0".as_ptr())) == 0 {
+        if notify_init("hello\0".as_ptr() as *const c_char) == 0 {
             panic!("Failed to init libnotify");
         }
 
-        let notif = notify_notification_new(tm("Hello\0".as_ptr()),
-                                            tm("World\0".as_ptr()),
-                                            0 as *const i8);
-        let mut error = 0 as *mut glib::GError;
+        let notif = notify_notification_new("Hello\0".as_ptr() as *const c_char,
+                                            "World\0".as_ptr() as *const c_char,
+                                            ptr::null());
+        let mut error = ptr::null_mut();
         notify_notification_show(notif, &mut error);
-        if error != 0 as *mut glib::GError {
+
+        if error != ptr::null_mut() {
             glib::g_error_free(error);
         }
 
